2c1c003a9e1d165f6bc1457fb68c6e04cd156cef
hoseinaghaei <h.aghaei.araei@gmail.com>
2024-02-09 02:21:40 +0330

    1 - test data replication 2- test index replication 3- test read crun job

--- Codes/SADProject/Broker/read/read_crun.py
+++ Codes/SADProject/Broker/read/read_crun.py
@@ -1,16 +1,15 @@
 import schedule
-import time
 from Codes.SADProject.Broker.file.read import Read
 
 
 def read_sample_data():
     partition = "3"
 
-    read_instance = Read(partition)
+    read_instance = Read(partition, 'http://localhost:5001')
 
     print(read_instance.read_data())
     # todo: send to subscriber
 
 
 def schedule_read():
-    job = schedule.every(2).seconds.do(read_sample_data)
+    job = schedule.every(5).seconds.do(read_sample_data)
