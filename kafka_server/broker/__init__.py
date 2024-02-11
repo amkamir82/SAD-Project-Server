03644ae8320928c404804f8d55216fd8fad7b2c1
Mahdi Gheidi <mohammadmahdi.gheidi@divar.ir>
2024-02-11 20:25:01 +0330

    Add hossein's heartbeat files

--- kafka_server/broker/__init__.py
+++ kafka_server/broker/__init__.py
@@ -1,20 +1,31 @@
 import threading
 import time
-import sys
-import os
+import schedule
+
+import os, sys
 
 BROKER_PROJECT_PATH = os.getenv("BROKER_PROJECT_PATH", "/app/")
 sys.path.append(os.path.abspath(BROKER_PROJECT_PATH))
 
+from read.read_cronjob import schedule_read
+from controller.coordinator import heartbeat
 from read.read_cronjob import *
 
-def second_thread_function():
+def schedule_read_thread():
     schedule_read()
     while True:
         schedule.run_pending()
         time.sleep(1)
 
 
-second_thread = threading.Thread(target=second_thread_function)
-second_thread.daemon = True
-second_thread.start()
+def send_heartbeat():
+    heartbeat()
+
+
+read_thread = threading.Thread(target=schedule_read_thread)
+read_thread.daemon = True
+read_thread.start()
+
+heartbeat_thread = threading.Thread(target=send_heartbeat)
+heartbeat_thread.daemon = True
+heartbeat_thread.start()
