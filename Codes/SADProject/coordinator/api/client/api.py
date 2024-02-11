ad917ab501da26c50a1233c1f2ce870d19d639e5
amkamir82 <amk_amir82@yahoo.com>
2024-02-11 17:30:41 +0330

    add return broker urls in api

--- Codes/SADProject/coordinator/api/client/api.py
+++ Codes/SADProject/coordinator/api/client/api.py
@@ -1,11 +1,14 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request, jsonify
 
 api_blueprint = Blueprint('api', __name__)
 
 
-@api_blueprint.route('/init_client', methods=['POST'])
+@api_blueprint.route('/init_client', methods=['GET'])
 def init_client():
-    return 'Hello from api!'
+    remote_addr = (request.headers.environ["REMOTE_ADDR"], request.headers.environ["REMOTE_ADDR"])
+    print(remote_addr)
+    tmp = ["http://127.0.0.1:8000", "http://127.0.0.1:8001", "http://127.0.0.1:8002"]
+    return jsonify(tmp)
 
 
 @api_blueprint.route('/subscribe', methods=['POST'])
