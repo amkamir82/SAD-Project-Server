3a3e9e966e3794c969b674f13366be684c674da9
amkamir82 <amk_amir82@yahoo.com>
2024-02-14 04:49:24 +0330

    fix bug of coordinator_database

--- kafka_server/coordinator_database/main.py
+++ kafka_server/coordinator_database/main.py
@@ -187,11 +187,12 @@ def list_of_replicas():
     with concurrent.futures.ThreadPoolExecutor() as executor:
         try:
             future = executor.submit(broker.list_of_replicas)
-            _ = future.result()
+            result = future.result()
         except Exception as e:
             logger.error(e)
             return jsonify("Error deleting broker heartbeat"), 500
-    return jsonify("Successfully deleting broker heartbeat"), 200
+    return jsonify(result), 200
+
 
 if __name__ == '__main__':
     app.run(port=5001)
