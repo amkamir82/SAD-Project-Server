209a6655bb3a3f8724b525b862add1885efd5bcb
amkamir82 <amk_amir82@yahoo.com>
2024-02-14 20:07:53 +0330

    fix bug of subscribe for broker

--- kafka_server/coordinator_database/services/broker.py
+++ kafka_server/coordinator_database/services/broker.py
@@ -43,9 +43,9 @@ def init_brokers_replicas_file():
         with open(path, 'w', encoding='utf8') as f:
             replicas = {
                 "replica": {
-                    "1": "http://185.226.116.193:5678",
-                    "2": "http://185.226.116.193:5679",
-                    "3": "http://185.226.116.193:5680"
+                    "1": "http://5.34.192.132:5678",
+                    "2": "http://5.34.192.132:5679",
+                    "3": "http://5.34.192.132:5680"
                 }
             }
             f.write(json.dumps(replicas))
