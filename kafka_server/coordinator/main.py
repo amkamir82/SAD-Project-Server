e479531f2b35fc6076e11b1f8199cd0b677ba55e
amkamir82 <amk_amir82@yahoo.com>
2024-02-14 03:52:07 +0330

    fix bug of import

--- kafka_server/coordinator/main.py
+++ kafka_server/coordinator/main.py
@@ -7,6 +7,7 @@ from api.client.api import api_blueprint as client_api
 from api.broker.api import api_blueprint as broker_api
 
 from coordinator.services.client import subscribe as client_subscribe_service
+from coordinator.services.broker import subscribe as broker_subscribe_service
 
 COORDINATOR_PROJECT_PATH = os.getenv("COORDINATOR_PROJECT_PATH", "/app/")
 sys.path.append(os.path.abspath(COORDINATOR_PROJECT_PATH))
