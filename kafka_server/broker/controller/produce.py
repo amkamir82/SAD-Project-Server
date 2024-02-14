a088139e7df73eb7221cf58222ff599f13fcec53
hoseinaghaei <h.aghaei.araei@gmail.com>
2024-02-14 05:25:43 +0330

    fix file name.

--- kafka_server/broker/controller/produce.py
+++ kafka_server/broker/controller/produce.py
@@ -126,7 +126,7 @@ def subscribers():
         data = json.loads(request.data.decode("utf-8"))
         brokers = data['subscribers']
 
-        subscribers_file_path = os.path.join(os.getcwd(), 'data', 'subscriptions', 'subscribers.json')
+        subscribers_file_path = os.path.join(os.getcwd(), '../data', 'subscriptions', 'subscribers.json')
 
         with open(subscribers_file_path, "w+") as file:
             json.dump(brokers, file)
